9cec53b160325ef33cef304737a4b6f7efe431b0
Besednik <filipbesednik@gmail.com>
2018-04-26 23:45:20 +0200

    kom

---
+++ program.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "game_of_life.h"
+#include "ctime"
+using namespace std;
+
+int main() {
+	game_of_life the_game;
+	srand(time(nullptr));
+
+	the_game.crtanje_generacije_1();
+
+	bool dalje;
+	cout << "Dalje (1/0): ";
+	cin >> dalje;
+	while (dalje)
+	{
+		the_game.iscrtaj();
+		the_game.sljedeca_generacija();
+		the_game.zamjena_generacija();
+
+		cout << "Dalje (1/0): ";
+		cin >> dalje;
+	}
+
+	return 0;
+}
